cabe36e921a7b5eeaa035dd1860c83917784649d
Lovász Ákos <akos4x44@gmail.com>
2021-04-15 13:45:01 +0200

    fixed ordering seeder for random orders
    made random amount of products in one order possible

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CategorySeeder::class);
         $this->call(ProductSeeder::class);
         $this->call(OrderSeeder::class);
+        $this->call(OrderingSeeder::class);
     }
 }

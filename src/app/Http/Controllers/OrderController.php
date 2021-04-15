e510973ce58bc9f0c46399a7670efe99ec1d8a19
Lovász Ákos <akos4x44@gmail.com>
2021-04-15 13:15:41 +0200

    added Order Model and Controller

---
+++ src/app/Http/Controllers/OrderController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+
+    public function index()
+    {
+        return view('order.index', ['order' => Order::all()]);
+    }
+
+
+    public function create()
+    {
+        return view('order.form');
+    }
+
+   
+    public function store(Request $request)
+    {
+        Order::create($request->validated());
+        return redirect(route('order.index'), 201);
+    }
+
+    
+    public function show(Order $order)
+    {
+        return view('order.show', ['order' => $order]);
+    }
+
+   
+    public function edit(Order $order)
+    {
+        return view('order.form', ['order' => $order]);
+    }
+
+   
+    public function update(Request $request, Order $order)
+    {
+        $order->update($request->validated());
+        $order->save();
+
+        return redirect(route('order.show', [$order->id]), 204);
+    }
+
+    
+    public function destroy(Order $order)
+    {
+        $order->delete();
+
+        return redirect(route('order.index'));
+    }
+}
---
+++ src/app/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\ApiResource;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory, ApiResource;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'address',
+        'zip_code',
+        'city',
+        'country',
+        'complete',
+        'order_time'
+    ];
+
+    public function products(){
+        return $this->belongsTo(Product::class)->WithPivot(['amount']);
+    }
+}

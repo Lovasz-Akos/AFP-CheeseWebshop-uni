36c4d85432f2306283dca35fabce2a4faa27fc6b
Dombi Tibor <dombidav@gmail.com>
2021-04-18 19:48:05 +0200

    Fixed Webpack to compile every SASS not just app.sass

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+const glob = require('glob');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -10,5 +10,16 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+//<editor-fold desc="Pattern matching for globify function">
+const files = pattern => glob.sync(pattern, { cwd: 'resources/sass' });
+
+const globify = (pattern, out, mixFunctionName) => {
+    files(pattern).forEach((path) => {
+        mix[mixFunctionName](`resources/sass/${path}`, out);
+    })
+};
+//</editor-fold>
+
 mix.sass('resources/sass/app.sass', 'public/css/app.css');
+globify('**/mix_*.sass', 'public/css', 'sass');
 mix.js('resources/js/*.js', 'public/js/app.js');

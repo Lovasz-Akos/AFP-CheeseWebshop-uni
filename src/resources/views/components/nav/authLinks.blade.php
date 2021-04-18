5e2b8e8aa2731cadf9fd17bd49763a8ab1ed4c21
Dombi Tibor <dombidav@gmail.com>
2021-04-18 20:03:48 +0200

    Fixed logout button not working

--- src/resources/views/components/nav/authLinks.blade.php
+++ src/resources/views/components/nav/authLinks.blade.php
@@ -1,29 +1,30 @@
 @guest
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('login') }}">Login</a>
-</li>
-@if (Route::has('register'))
-<li class="nav-item">
-    <a class="nav-link" href="{{ route('register') }}">Register</a>
-</li>
-@endif
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">Login</a>
+    </li>
+    @if (Route::has('register'))
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('register') }}">Register</a>
+    </li>
+    @endif
 @else
-<li class="nav-item dropdown">
-    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        {{ Auth::user()->name }}
-    </a>
+    <li class="nav-item">
+        <div class="dropdown">
+            <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                {{ Auth::user()->name }}
+            </button>
 
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-        <!--suppress JSDeprecatedSymbols : jQuery has a problem with event.preventDefault(). TODO: (low) Fix it if you have time. -->
-        <a class="dropdown-item" href="{{ route('logout') }}"
-           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-            Logout
-        </a>
+            <!-- TODO: Fix white characters! -->
+            <ul class="dropdown-menu text-dark" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">Action</a></li> <!-- TODO: Remove examples if not needed -->
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><input class="dropdown-item" type="submit" form="logout-form" value="Logout" /></li>
+            </ul>
+        </div>
+    </li>
 
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-        </form>
-    </div>
-</li>
+    <form id="logout-form" name="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf
+    </form>
 @endguest

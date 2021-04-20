20849daa79923ce5bc256c0274e491aae1d15618
Krigovszki Bálint <krigovszki.balint@gmail.com>
2021-04-20 22:06:07 +0200

    Added CSS for Forgot Password UI

--- src/resources/views/auth/passwords/email.blade.php
+++ src/resources/views/auth/passwords/email.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.app')
 
+@push('css')
+    <link href="{{ asset('css/login-register.css') }}" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet">
+@endpush
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
+                <div class="input-custom">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -16,11 +23,11 @@
 
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
-
+                        <label for="email" class="col-md-6 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -30,15 +37,17 @@
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="login-btn">
+                        <div class="form-group row mb-10">
+                            <div class="col-md-12">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
                         </div>
+                        </div>
                     </form>
+                </div>
                 </div>
             </div>
         </div>

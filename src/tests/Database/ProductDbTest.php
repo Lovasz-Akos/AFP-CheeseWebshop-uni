3d4b9d952fce996474d216130756d769f6a087dc
Boldizsár Szenczi <boldi.szenczi@gmail.com>
2021-05-19 19:53:34 +0200

    Modified ProductDbTest
    added test subject and connection

--- src/tests/Database/ProductDbTest.php
+++ src/tests/Database/ProductDbTest.php
@@ -1,38 +1,42 @@
 <?php
 
+
 namespace Tests\Database;
 
+
 use App\Models\Product;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Tests\TestCase;
 
 class ProductDbTest extends TestCase
 {
-    public function testProductDatabaseConnection(): void
-    {
+    public function testProductDatabaseConnection(): void{
         try{
             $result = Product::all();
             self::assertNotNull($result);
-        }catch (\Exception $exception){
-            self::fail($exception->getMessage());
+        }catch (\Exception $e){
+            self::fail($e->getMessage());
         }
     }
 
-    public function testProductStructure(): void
-    {
+    public function testProductStructure(): void{
         $subject = Product::random();
 
+        self::assertNotNull($subject->id);
         self::assertNotNull($subject->brand);
-        
+        self::assertNotNull($subject->name);
+        self::assertNotNull($subject->price);
+        self::assertNotNull($subject->in_stock);
+        self::assertNotNull($subject->description);
+        self::assertNotNull($subject->short_description);
+        self::assertNotNull($subject->created_at);
+        self::assertNotNull($subject->updated_at);
     }
 
-    public function testProductModelConnections(): void
-    {
+    public function testProductModelConnections(): void{
         $subject = Product::random();
 
-        self::assertNotNull($subject->product);
-
-        self::assertInstanceOf(BelongsTo::class, $subject->product());
+        self::assertNotNull($subject->category);
+        self::assertInstanceOf(BelongsTo::class, $subject->category());
     }
 }

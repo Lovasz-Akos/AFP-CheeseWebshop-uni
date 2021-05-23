30a452865737498157ea07ae9b0d4ebda4d5cdc8
Boldizsár Szenczi <boldi.szenczi@gmail.com>
2021-05-23 15:18:53 +0200

    Added Order and User counter to Admin Home

--- src/app/Http/Controllers/StaticController.php
+++ src/app/Http/Controllers/StaticController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\User;
 use Auth;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\{Factory as ViewFactory, View};
@@ -17,7 +19,11 @@ class StaticController extends Controller
     public function home(): ViewFactory|View|Application
     {
         if(Auth::user()->is_admin) {
-            return view('home');
+            return view('home', [
+                'order_count' => Order::where('status', '<', '1')->count(),
+                'register_count' => User::whereDate('created_at', '<=', now()->subDays(7)->startOfDay()->toDateTimeString())->get()
+                //startOfDay() is needed, because now it starts counting from midnight, not the current time. Without this you need to write subDay(8), which is fine I guess, but less obvious
+            ]);
         }
 
         return redirect(route('product.index'));

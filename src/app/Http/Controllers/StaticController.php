5845c3ec2eebb3c534392ce69e64479362b4c730
Boldizsár Szenczi <boldi.szenczi@gmail.com>
2021-05-23 15:13:34 +0200

    Redirect simple users away from admin dashboard

--- src/app/Http/Controllers/StaticController.php
+++ src/app/Http/Controllers/StaticController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\{Factory as ViewFactory, View};
+use Illuminate\Http\RedirectResponse;
 
 class StaticController extends Controller
 {
-    public function welcome(): ViewFactory|View|Application
+    public function welcome(): ViewFactory|View|Application|RedirectResponse
     {
         return view('welcome');
     }
 
     public function home(): ViewFactory|View|Application
     {
-        return view('home');
+        if(Auth::user()->is_admin) {
+            return view('home');
+        }
+
+        return redirect(route('product.index'));
     }
 }

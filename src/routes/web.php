108658b094beb323dea7bb4eae5f8dddba77a31f
Boldizsár Szenczi <boldi.szenczi@gmail.com>
2021-05-23 15:23:39 +0200

    Added User resource to routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,6 +24,7 @@ Route::group(['middleware' => 'auth'], function ($router){
     Route::resource('category', CategoryController::class)->only(['create', 'edit']);
     Route::resource('product', ProductController::class)->only(['create', 'edit']);
     Route::resource('order', OrderController::class);
+    Route::resource('user', UserController::class)->except('create');
 });
 
 Route::resource('category', CategoryController::class)->except(['create', 'edit']);

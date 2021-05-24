f201a27c6eb1af260f0332451d88b0bb1c911aea
Krigovszki Bálint <krigovszki.balint@gmail.com>
2021-05-25 01:59:58 +0200

    Added password requirement for profile edit

--- src/app/Http/Requests/UserRequest.php
+++ src/app/Http/Requests/UserRequest.php
@@ -15,10 +15,14 @@ class UserRequest extends FormRequest
 
     public function rules(): array
     {
-        return [
+        $validationRules = [
             'name' => ['required', 'min:3'],
             'email' => ['required', 'email', Rule::unique('users')->ignore(Auth::user()->id)], //Since only the logged in user can edit their OWN profile this is fine
-            'password' => [
+            'current_password' => ['required', 'password:web']
+        ];
+
+        if($this->has('password') && !empty($this->get('password'))){
+            $validationRules['password'] = [
                 'confirmed',          // this means there is a password_confirmation input, which must match this
                 'string',             // regex is buggy without specifying the type, probably a bug with the validation engine
                 'min:10',             // must be at least 10 characters in length
@@ -26,7 +30,8 @@ class UserRequest extends FormRequest
                 'regex:/[A-Z]/',      // must contain at least one uppercase letter
                 'regex:/[0-9]/',      // must contain at least one digit
                 'regex:/[@$!%*#?&]/', // must contain a special character
-            ],
-        ];
+            ];
+        }
+        return $validationRules;
     }
 }

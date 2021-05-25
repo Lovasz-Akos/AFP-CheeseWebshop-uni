ea67b21d8dfad154994091e583f0d744a1decab5
Krigovszki Bálint <krigovszki.balint@gmail.com>
2021-05-26 01:12:03 +0200

    Minor changes in product details page and CSS

--- src/resources/views/product/show.blade.php
+++ src/resources/views/product/show.blade.php
@@ -9,12 +9,12 @@
 @section('content')
     <div class="container">
         <div class="wrapper card">
+        <div class="card-head">{{ $product->name }}</div>
             <div class="row mx-1 mx-lg-4">
                 <div class="col-12 col-lg-4">
                     <img src="{{ asset('img/product_img.png') }}" alt="">
                 </div>
                 <div class="col-12 col-lg-8">
-                    <h2 class="m-lg-3">{{ $product->name }}</h2>
                     <div class="my-lg-3">{{--  Spacer  --}}&nbsp;&nbsp;</div>
                     <x-table.details class="m-1 m-lg-4"
                                      tdClass="p-1 px-3"

eb09827b33b16b8e8f51718662a59c9736fc31fd
Boldizsár Szenczi <boldi.szenczi@gmail.com>
2021-05-27 00:24:25 +0200

    Backend: Added Order Validation rules, and linked it to Controller

---
+++ src/app/Http/Requests/OrderRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class OrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => ['required', 'min:3'],
+            'last_name' => ['required', 'min:3'],
+            'address' => ['required', 'min:3'],
+            'city' => ['required'],
+            'zip_code' => ['required'],
+            'country' => ['required'],
+            'product' => [Rule::exists('orders', 'id')]
+        ];
+    }
+}
